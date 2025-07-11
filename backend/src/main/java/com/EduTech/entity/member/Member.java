67a9ed08152af1562de4cdbfefabde205dbe778d
Nam-yongjin <oj4263@naver.com>
2025-07-11 10:35:12 +0900

    first backend

---
+++ backend/src/main/java/com/EduTech/entity/member/Member.java
@@ -0,0 +1,17 @@
+package com.EduTech.entity.member;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Member {
+
+	
+}
