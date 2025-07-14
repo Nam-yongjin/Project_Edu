8a49acd0bc0f46f9620f943f43b97bdb28ff8ff0
miyeon Kim <aldus9714@naver.com>
2025-07-14 19:24:04 +0900

    QnaEntity 컬럼

--- backend/src/main/java/com/EduTech/entity/qna/Answer.java
+++ backend/src/main/java/com/EduTech/entity/qna/Answer.java
@@ -1,14 +1,9 @@
 package com.EduTech.entity.qna;
 
 import com.EduTech.entity.BaseEntity;
-import com.EduTech.entity.notice.NoticeFile;
+import com.EduTech.entity.member.Member;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,7 +16,18 @@ import lombok.NoArgsConstructor;
 public class Answer extends BaseEntity{
 
 	@Id
-	@Column(nullable = false)
+	@Column(nullable = false) //답변글번호
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long answerNum;
+	
+	@Column(nullable = false) //내용
+	private String content;
+	
+	@ManyToOne //하나의 질문글에 여러 답변이 가능
+	@JoinColumn(name = "questionNum", nullable = false) //질문글번호
+	private Question question;
+	
+	@ManyToOne //여러 답변글을 하나의 회원이 작성
+	@JoinColumn(name = "memId", nullable = false) //회원아이디
+	private Member member;
 }
--- backend/src/main/java/com/EduTech/entity/qna/Question.java
+++ backend/src/main/java/com/EduTech/entity/qna/Question.java
@@ -1,13 +1,9 @@
 package com.EduTech.entity.qna;
 
 import com.EduTech.entity.BaseEntity;
+import com.EduTech.entity.member.Member;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,7 +16,24 @@ import lombok.NoArgsConstructor;
 public class Question extends BaseEntity{
 
 	@Id
-	@Column(nullable = false)
+	@Column(nullable = false) //질문글번호
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long questionNum;
+	
+	@Column(nullable = false) //제목
+	private String title;
+	
+	@Column(nullable = false) //내용
+	private String content;
+	
+	@Enumerated(EnumType.STRING) //공개여부
+	@Column(nullable = false)
+	private QnaState state;
+	
+	@Column(nullable = false) //조회수
+	private Long view;	
+	
+	@ManyToOne //여러 질문글을 하나의 회원이 작성
+	@JoinColumn(name = "memId", nullable = false) //회원아이디
+	private Member member;
 }
