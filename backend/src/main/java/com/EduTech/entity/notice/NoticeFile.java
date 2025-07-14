5989ea93a1abbffaba89e81068398aaea6572dc0
miyeon Kim <aldus9714@naver.com>
2025-07-14 18:45:42 +0900

    Merge branch 'Event' into main

--- backend/src/main/java/com/EduTech/entity/notice/NoticeFile.java
+++ backend/src/main/java/com/EduTech/entity/notice/NoticeFile.java
@@ -1,11 +1,6 @@
 package com.EduTech.entity.notice;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +13,20 @@ import lombok.NoArgsConstructor;
 public class NoticeFile {
 
 	@Id
-	@Column(nullable = false)
+	@Column(nullable = false) //첨부파일번호
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long notFileNum;
+	
+	@Column(nullable = false) //원본파일명
+	private String originalName;
+	
+	@Column(nullable = false) //파일저장경로
+	private String filePath;
+	
+	@Column(nullable = false) //파일종류
+	private String fileType;
+	
+	@ManyToOne(fetch = FetchType.LAZY) //여러 개의 파일을 하나의 공지사항에 첨부
+	@JoinColumn(name = "noticeNum", nullable = false) //공지사항번호
+	private Notice notice;
 }
