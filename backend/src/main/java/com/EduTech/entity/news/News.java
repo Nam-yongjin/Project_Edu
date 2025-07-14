2d9c42698b39aff3cbaca5fa6670c3d3f02a7888
miyeon Kim <aldus9714@naver.com>
2025-07-14 18:20:16 +0900

    NewsEntity 컬럼

--- backend/src/main/java/com/EduTech/entity/news/News.java
+++ backend/src/main/java/com/EduTech/entity/news/News.java
@@ -1,14 +1,9 @@
 package com.EduTech.entity.news;
 
 import com.EduTech.entity.BaseEntity;
-import com.EduTech.entity.facility.FacilityTime;
+import com.EduTech.entity.member.Member;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,7 +16,22 @@ import lombok.NoArgsConstructor;
 public class News extends BaseEntity{
 
 	@Id
-	@Column(nullable = false)
+	@Column(nullable = false) //뉴스번호
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long newsNum;
+	
+	@Column(nullable = false) //제목
+	private String title;
+	
+	@Column(nullable = false) //내용
+	private String content;
+	
+	@Column(nullable = false) //조회수
+	private Long view;	
+	
+	@ManyToOne //여러 개의 언론보도 게시글을 한 명의 Admin이 작성 가능
+	@JoinColumn(name = "memId", nullable = false)
+	private Member member;
+
+	
 }
