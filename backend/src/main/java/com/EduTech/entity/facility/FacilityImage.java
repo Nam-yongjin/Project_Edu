35cd16992f463252a7079d0efd8c518158b9775d
miyeon Kim <aldus9714@naver.com>
2025-07-14 17:49:37 +0900

    FacilityEntity 컬럼

--- backend/src/main/java/com/EduTech/entity/facility/FacilityImage.java
+++ backend/src/main/java/com/EduTech/entity/facility/FacilityImage.java
@@ -1,11 +1,6 @@
 package com.EduTech.entity.facility;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +13,19 @@ import lombok.NoArgsConstructor;
 public class FacilityImage {
 
 	@Id
-	@Column(nullable = false)
+	@Column(nullable = false) //이미지번호
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long facImageNum;
+	
+	@Column(nullable = false) //이미지이름
+	private String imageName;
+	
+	@Column(nullable = false) //이미지URL
+	private String imageUrl;
+	
+	@ManyToOne(fetch = FetchType.LAZY) //하나의 시설에 여러 개의 이미지 가능
+	@JoinColumn(name = "facilityNum", nullable = false) //장소번호
+	private Facility facility;
+	
+	
 }
--- backend/src/main/java/com/EduTech/entity/facility/FacilityReserve.java
+++ backend/src/main/java/com/EduTech/entity/facility/FacilityReserve.java
@@ -1,11 +1,10 @@
 package com.EduTech.entity.facility;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+import com.EduTech.entity.member.Member;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +17,30 @@ import lombok.NoArgsConstructor;
 public class FacilityReserve {
 
 	@Id
-	@Column(nullable = false)
+	@Column(nullable = false) //예약신청번호
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long facRevNum;
+	
+	@Column(nullable = false) //신청일
+	private LocalDateTime reserveAt;
+	
+	@Column(nullable = false) //이용시작일
+	private LocalDateTime startDate;
+	
+	@Column(nullable = false) //이용종료일
+	private LocalDateTime endDate;
+	
+	@Enumerated(EnumType.STRING) //상태
+	@Column(nullable = false)
+	private FacilityState state;
+	
+	@ManyToOne(fetch = FetchType.LAZY) //하나의 시설에 여러 개의 예약 가능
+	@JoinColumn(name = "facilityNum", nullable = false) //장소번호
+	private Facility facility;
+	
+	@ManyToOne(fetch = FetchType.LAZY) //하나의 시설에 여러 명의 회원이 예약 가능
+	@JoinColumn(name = "memId", nullable = false) //회원아이디
+	private Member member;
+	
+	
 }
