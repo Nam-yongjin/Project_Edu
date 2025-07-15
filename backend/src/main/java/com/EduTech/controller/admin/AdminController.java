96d8e122dd82840d5a4cf7c667aa78ccfc4baf76
Nam-yongjin <oj4263@naver.com>
2025-07-15 09:51:08 +0900

    Controller

---
+++ backend/src/main/java/com/EduTech/controller/admin/AdminController.java
@@ -0,0 +1,13 @@
+package com.EduTech.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+
+}
---
+++ backend/src/main/java/com/EduTech/controller/event/EventController.java
@@ -0,0 +1,13 @@
+package com.EduTech.controller.event;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/event")
+public class EventController {
+
+}
