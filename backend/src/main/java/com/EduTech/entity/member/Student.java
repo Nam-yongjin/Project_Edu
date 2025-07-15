f435893f1708e09d9aafe8d20746c2b880afd793
miyeon Kim <aldus9714@naver.com>
2025-07-15 09:55:29 +0900

    MemberEntity 컬럼 (company, student, teacher)

--- backend/src/main/java/com/EduTech/entity/member/Student.java
+++ backend/src/main/java/com/EduTech/entity/member/Student.java
@@ -1,5 +1,14 @@
 package com.EduTech.entity.member;
 
-public class Student {
+import jakarta.persistence.*;
 
+public class Student {
+	
+	@Column(nullable = false) //학교
+	private String schoolName;
+	
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "memId") //회원아이디
+	private Member member;
 }
