b2b0e9bc939c1f57ca185341be9dabc4e91ec677
Nam-yongjin <oj4263@naver.com>
2025-07-15 11:22:17 +0900

    Entity 수정

--- backend/src/main/java/com/EduTech/entity/facility/FacilityTime.java
+++ backend/src/main/java/com/EduTech/entity/facility/FacilityTime.java
@@ -2,15 +2,23 @@ package com.EduTech.entity.facility;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "facility_time")
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 @Data
 public class FacilityTime {
 
