19c88bd721a32e7a8ed40ce79a6e7cb31591878e
Nam-yongjin <oj4263@naver.com>
2025-07-15 10:31:47 +0900

    DTO example

--- backend/src/main/java/com/EduTech/dto/member/MemberDTO.java
+++ backend/src/main/java/com/EduTech/dto/member/MemberDTO.java
@@ -1,8 +1,49 @@
 package com.EduTech.dto.member;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.EduTech.entity.member.MemberRole;
+
 import lombok.Data;
 
 @Data
-public class MemberDTO {
+public class MemberDTO extends User { // User은 인증된 사용자 정보 담는 객체이므로 MemberDTO에서만 사용
+
+	private String memId;
+
+	private String pw;
+
+	private String name;
+
+	private String email;
+
+	private MemberRole role;
+
+	// 생성자
+	public MemberDTO(String memId, String pw, String name, String email, MemberRole role) {
+		super(memId, pw, List.of(new SimpleGrantedAuthority("ROLE_" + role)));
+		this.memId = memId;
+		this.pw = pw;
+		this.name = name;
+		this.email = email;
+		this.role = role;
+	}
+
+	// JWT 등을 쓸 때 사용자의 정보를 Map으로 추출
+	public Map<String, Object> getClaims() {
+		Map<String, Object> dataMap = new HashMap<>();
+		dataMap.put("memId", memId);
+		dataMap.put("pw", pw);
+		dataMap.put("name", name);
+		dataMap.put("email", email);
+		dataMap.put("role", role);
+
+		return dataMap;
+	}
 
 }
