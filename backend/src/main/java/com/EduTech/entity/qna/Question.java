b4d948740fd8184f96a3d95f95d37ead76db8235
miyeon Kim <aldus9714@naver.com>
2025-07-15 10:06:13 +0900

    Qna 관계설정 추가

--- backend/src/main/java/com/EduTech/entity/qna/Question.java
+++ backend/src/main/java/com/EduTech/entity/qna/Question.java
@@ -1,6 +1,10 @@
 package com.EduTech.entity.qna;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.EduTech.entity.BaseEntity;
+import com.EduTech.entity.event.EventFile;
 import com.EduTech.entity.member.Member;
 
 import jakarta.persistence.*;
@@ -36,4 +40,7 @@ public class Question extends BaseEntity{
 	@ManyToOne //여러 질문글을 하나의 회원이 작성
 	@JoinColumn(name = "memId", nullable = false) //회원아이디
 	private Member member;
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.ALL) //하나의 질문에 여러 개의 답변 가능, 질문 삭제되면 답변도 같이 삭제
+	private List<Answer> answer = new ArrayList<>();
 }
