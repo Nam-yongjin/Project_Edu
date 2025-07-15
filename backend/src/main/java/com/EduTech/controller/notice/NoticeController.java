1b267df8b852df3123a5f4ac16f49d48b3ace784
Nam-yongjin <oj4263@naver.com>
2025-07-15 09:51:08 +0900

    Controller

---
+++ backend/src/main/java/com/EduTech/controller/notice/NoticeController.java
@@ -0,0 +1,13 @@
+package com.EduTech.controller.notice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notice")
+public class NoticeController {
+
+}
---
+++ backend/src/main/java/com/EduTech/controller/qna/QuestionController.java
@@ -0,0 +1,13 @@
+package com.EduTech.controller.qna;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/question")
+public class QuestionController {
+
+}
