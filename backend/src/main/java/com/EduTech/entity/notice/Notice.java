6da22e8b91956c5b2ae5ba9b73cccbc0131f3858
miyeon Kim <aldus9714@naver.com>
2025-07-15 20:20:25 +0900

    Notice: Entity에 고정 기능 추가, DTO랑 Repository작성

--- backend/src/main/java/com/EduTech/entity/notice/Notice.java
+++ backend/src/main/java/com/EduTech/entity/notice/Notice.java
@@ -3,20 +3,31 @@ package com.EduTech.entity.notice;
 import com.EduTech.entity.BaseEntity;
 import com.EduTech.entity.member.Member;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Entity
 @Table(name = "notice")
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED) //Builder 패턴이나 팩토리 메서드를 통해 객체 생성을 통제하고 데이터 무결성 보장
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //JPA 내부에서만 기본 생성자 호출을 허용하여 불완전한 객체 생성을 방지
 @Data
 public class Notice extends BaseEntity{
 
 	@Id
-	@Column(nullable = false) //공지사항번호
+	@Column(nullable = false) //공지사항번호(PK)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long noticeNum;
 	
@@ -26,10 +37,15 @@ public class Notice extends BaseEntity{
 	@Column(nullable = false) //내용
 	private String content;
 	
-	@Column(nullable = false) //조회수
-	private Long view;	
+	@Builder.Default //Builder 사용할 때 기본 값 유지(false)
+	@Column(nullable = false)
+	private boolean isPinned = false; //고정여부
 	
-	@ManyToOne //여러 개의 공지사항 게시글을 한 명의 Admin이 작성 가능
+	@Builder.Default
+	@Column(nullable = false) //조회수
+	private Long view = 0L;	//0으로 초기화 해서 Null값 방지
+	
+	@ManyToOne(fetch = FetchType.LAZY) //여러 개의 공지사항 게시글을 한 명의 Admin이 작성 가능, 지연로딩
 	@JoinColumn(name = "memId", nullable = false) //회원아이디
 	private Member member;
 	
