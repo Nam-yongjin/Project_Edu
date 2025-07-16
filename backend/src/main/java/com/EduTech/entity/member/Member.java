110238d447407e3a080fa00fed258874f0cacc30
Nam-yongjin <oj4263@naver.com>
2025-07-16 16:52:11 +0900

    유효성검사 수정

--- backend/src/main/java/com/EduTech/entity/member/Member.java
+++ backend/src/main/java/com/EduTech/entity/member/Member.java
@@ -26,53 +26,36 @@ import lombok.NoArgsConstructor;
 public class Member extends BaseEntity{
 
 	@Id
-	@Column(length = 16, nullable = false)
-//	@Size(min = 6, max = 16, message = "6자 이상 16자 이하로 입력해주세요.")
-//	@Pattern(regexp = "^[A-Za-z0-9]{6,16}$", message = "영문 대소문자 또는 숫자만 입력 가능합니다.")
 	private String memId;
 	
-	@Column(nullable = false)
-//	@Size(min = 6, max = 16, message = "6자 이상 16자 이하로 입력해주세요.")
-//	@Pattern(regexp = "^[A-Za-z0-9!@#$.]{6,16}$", message = "영문 대소문자, 숫자, 특수문자(!@#$.)만 사용 가능합니다.")
 	private String pw;
 	
-	@Column(length = 6, nullable = false)
-//	@Size(max = 6, message = "6자 이하로 입력해주세요.")
-//	@Pattern(regexp = "^[가-힣]*$", message = "한글만 입력 가능합니다.")
 	private String name;
 	
-	@Column(length = 100, nullable = false, unique=true)
+	@Column(unique = true)
 	private String email;
 	
-	@Column(nullable = false)
 	private LocalDate birthDate;
 	
-	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private MemberGender gender;
 	
-	@Column(length = 11, nullable = false, unique=true)
-//	@Size(min = 10, max = 11)
-//	@Pattern(regexp = "^01[016789][0-9]{7,8}$", message = "유효한 휴대전화 번호를 입력하세요.")
+	@Column(unique = true)
 	private String phone;
 	
-	@Column(nullable = false)
 	private String addr;
 	
-	@Column(nullable = false)
 	private boolean	checkSms;
 	
-	@Column(nullable = false)
 	private boolean checkEmail;
 	
-	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private MemberState state;
 	
-	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private MemberRole role;
 	
+	@Column(unique=true)
 	private String kakao;
 	
 	// 회원 삭제시 연결된 추가정보 자동 삭제
