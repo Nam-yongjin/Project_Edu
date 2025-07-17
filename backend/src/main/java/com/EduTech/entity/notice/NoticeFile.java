3e2c453bdddb952c7a4898537644ebe6e85da01c
miyeon Kim <aldus9714@naver.com>
2025-07-17 15:40:02 +0900

    Notice 유효성검사

--- backend/src/main/java/com/EduTech/entity/notice/NoticeFile.java
+++ backend/src/main/java/com/EduTech/entity/notice/NoticeFile.java
@@ -1,14 +1,7 @@
 package com.EduTech.entity.notice;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,21 +16,16 @@ import lombok.NoArgsConstructor;
 public class NoticeFile {
 
 	@Id
-	@Column(nullable = false) //첨부파일번호
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long notFileNum;
+	private Long notFileNum; //첨부파일번호
+
+	private String originalName; //원본파일명
 	
-	@Column(nullable = false) //원본파일명
-	private String originalName;
+	private String filePath; //파일저장경로
 	
-	@Column(nullable = false) //파일저장경로
-	private String filePath;
-	
-	@Column(nullable = false) //파일종류
-	private String fileType;
+	private String fileType; //파일종류
 	
 	@ManyToOne(fetch = FetchType.LAZY) //여러 개의 파일을 하나의 공지사항에 첨부
-	@JoinColumn(name = "noticeNum", nullable = false) //공지사항번호
-	private Notice notice;
+	@JoinColumn(name = "noticeNum")
+	private Notice notice; //공지사항번호
 	
 }
