86ad4379b02971b6ef9cbc7fa5e0684563451ebe
miyeon Kim <aldus9714@naver.com>
2025-07-17 16:54:00 +0900

    News 유효성검사, url 추가

--- backend/src/main/java/com/EduTech/dto/news/NewsUpdateDTO.java
+++ backend/src/main/java/com/EduTech/dto/news/NewsUpdateDTO.java
@@ -2,6 +2,8 @@ package com.EduTech.dto.news;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
@@ -15,8 +17,8 @@ public class NewsUpdateDTO {
 	
 	private String memId; //회원아이디
 	
-	private List<String> urlList; //새 파일
+	private List<String> oldFiles; //기존 파일목록
 	
-	private List<String> oldFiles; //남길 파일 목록
+	private List<MultipartFile> newFiles; //새로 업로드 할 파일
 
 }
--- backend/src/main/java/com/EduTech/entity/news/News.java
+++ backend/src/main/java/com/EduTech/entity/news/News.java
@@ -6,16 +6,7 @@ import java.util.List;
 import com.EduTech.entity.BaseEntity;
 import com.EduTech.entity.member.Member;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,25 +22,21 @@ import lombok.NoArgsConstructor;
 public class News extends BaseEntity{
 
 	@Id
-	@Column(nullable = false) //뉴스번호
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long newsNum;
+	private Long newsNum; //뉴스번호(PK)
 	
-	@Column(nullable = false) //제목
-	private String title;
+	private String title; //제목
 	
-	@Column(nullable = false) //내용
-	private String content;
+	private String content; //내용
 	
 	@Builder.Default
-	@Column(nullable = false) //조회수
-	private Long view = 0L;	//0으로 초기화 해서 Null값 방지	
+	private Long view = 0L;	//0으로 초기화 해서 Null값 방지
 	
 	@ManyToOne //여러 개의 언론보도 게시글을 한 명의 Admin이 작성 가능
 	@JoinColumn(name = "memId", nullable = false)
 	private Member member;
 	
-	@OneToMany(mappedBy = "news", cascade = CascadeType.ALL, orphanRemoval = true) //하나의 기사에 여러 개의 파일 첨부 가능, 기사가 삭제되면 파일도 같이 삭제
+	//하나의 기사에 여러 개의 파일 첨부 가능, 기사가 삭제되면 파일도 같이 삭제
+	@OneToMany(mappedBy = "news", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<NewsFile> newsFile = new ArrayList<>();
 	
 }
