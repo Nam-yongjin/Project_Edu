c5f9424e29c7f15832ab66aa58936c09447624cd
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-18 15:30:28 +0900

    ㅁ

--- backend/src/main/java/com/EduTech/repository/event/EventUseRepository.java
+++ backend/src/main/java/com/EduTech/repository/event/EventUseRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.EduTech.entity.event.Event;
 
-public interface EventRepository extends JpaRepository<Event, Long>{
+public interface EventUseRepository extends JpaRepository<Event, Long>{
 
 }
