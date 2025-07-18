6d114849a578ee233d894960ac204cf003a59b97
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-18 17:11:07 +0900

    시설 기본 정보

--- backend/src/main/java/com/EduTech/dto/facility/FacilityDTO.java
+++ backend/src/main/java/com/EduTech/dto/facility/FacilityDTO.java
@@ -1,8 +1,22 @@
 package com.EduTech.dto.facility;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FacilityDTO {
 
+    private Long facilityNum;	// 시설 번호
+    private String facName;     // 시설명
+    private String facInfo;     // 소개
+    private String category;    // 분류 (CONFERENCE, SEMINAR)
+    private int capacity;       // 수용 인원
+    private String facItem;     // 구비 품목
+    private String etc;         // 기타 유의사항
+    
 }
