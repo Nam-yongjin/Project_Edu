e50fd337f25d74de8e779e066e56b08b7892fa86
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-18 10:24:35 +0900

    Event

--- backend/src/main/java/com/EduTech/dto/event/EventDTO.java
+++ backend/src/main/java/com/EduTech/dto/event/EventDTO.java
@@ -1,8 +1,14 @@
 package com.EduTech.dto.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDTO {
-
+	private Long evRevNum;	//행사신천번호
 }
