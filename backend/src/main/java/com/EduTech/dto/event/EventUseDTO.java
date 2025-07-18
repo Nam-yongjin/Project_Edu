88488087e0052835d19a2c8c4a59e8eff5400c40
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-18 11:04:42 +0900

    행사관리자 상세 페이지 구현(행사 상세내용 및 신청회원정보)

--- backend/src/main/java/com/EduTech/dto/event/EventUseDTO.java
+++ backend/src/main/java/com/EduTech/dto/event/EventUseDTO.java
@@ -11,7 +11,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EventDTO {
-	private Long evRevNum;	//행사신천번호
-	private LocalDateTime applyAt;	// 행사신청일
+public class EventUseDTO {
+	private Long evRevNum;					// 행사신청번호
+	private LocalDateTime applyAt;			// 행사신청일
+	private String eventName;				// 행사명
+	private LocalDateTime applyPeriod;		// 신청기간
+	private LocalDateTime progressPeriod;	// 진행기간
+	private String place;					// 장소
+	private int totCapacity;				// 모집인원
+	private int currCapacity;				// 현재인원
+	private boolean state;					// 신청상태
+	
+	private Long eventNum;	// 행사 아이디
+	private String memid;	// 회원 아이디
+	private String naem;	// 회원 이름
+	private String email;	// 회원 이메일
+	private String phone;	// 회원 전화번호
 }
