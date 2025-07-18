bd3ca1cbd5381d67e493eb5f4f33e9f53c2af875
kimtaegun <tee1694@naver.com>
2025-07-18 14:58:49 +0900

    Demonstration

---
+++ backend/src/main/java/com/EduTech/dto/demonstration/DemonstrationReservationDTO.java
@@ -0,0 +1,10 @@
+package com.EduTech.dto.demonstration;
+
+import java.time.LocalDate;
+
+public class DemonstrationReservationDTO { // 실증 상품 상세 페이지에서 날짜 선택 후 예약 신청 버튼 클릭 시, 예약 정보를 받는 dto
+	private Long demNum; // 상품 번호
+	private LocalDate startDate; // 시작 날짜
+	private LocalDate endDate; // 끝 날짜
+	private String memId; // 신청자 아이디
+}
