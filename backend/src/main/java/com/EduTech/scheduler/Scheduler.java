de75889549b59caccd0645649b96c0ef6ce79ab0
Nam-yongjin <oj4263@naver.com>
2025-07-21 19:22:38 +0900

    회원정보 조회, 수정 repository, service

--- backend/src/main/java/com/EduTech/scheduler/Scheduler.java
+++ backend/src/main/java/com/EduTech/scheduler/Scheduler.java
@@ -13,7 +13,7 @@ public class Scheduler {
 	
 	private final MemberRepository memberRepository;
 
-	// 회원탈퇴 한달지난 회원정보 자동삭제
+	// 회원탈퇴 일주일지난 회원정보 자동삭제
     @Scheduled(cron = "0 0 0 * * *") // 매일 0시에 실행
     public void cleanUpLeaveMembers() {
         memberRepository.deleteMembersAfterOneMonthLeave();
