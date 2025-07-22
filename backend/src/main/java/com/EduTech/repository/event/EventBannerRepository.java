cd9879912e8403a3dcb93fcab4d5f5fedee2399d
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-22 15:15:25 +0900

    repository 리팩토링

--- backend/src/main/java/com/EduTech/repository/event/EventBannerRepository.java
+++ backend/src/main/java/com/EduTech/repository/event/EventBannerRepository.java
@@ -8,25 +8,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.EduTech.entity.event.EventFile;
+import com.EduTech.entity.event.EventBanner;
 
-public interface EventBannerRepository extends JpaRepository<EventFile, Long> {
+public interface EventBannerRepository extends JpaRepository<EventBanner, Long> {
 	
 	boolean existsByEvent_eventNum(Long eventNum);	//eventNum의 해당하는 행사 중복 등록 방지
 
-	Optional<EventFile> findByEvent_eventNum(Long eventNum);	//eventNum을 통해 EventFile 정보 가져옴
+	Optional<EventBanner> findByEvent_eventNum(Long eventNum);	//eventNum을 통해 EventFile 정보 가져옴
 
 	// 오늘을 기준으로 진행중이거나 아직 종료되지 않은 행사 표시
 	@Query("""
-			    SELECT pb FROM EventFile pb
-			    WHERE pb.event.progressEndPeriod >= :today
+			    SELECT pb FROM EventBanner pb
+			    WHERE pb.eventInfo.eventEndPeriod >= :today
 			""")
-	List<EventFile> findActiveEventBanners(@Param("today") LocalDate today);
+	List<EventBanner> findActiveEventBanners(@Param("today") LocalDate today);
 
 	// MAX 3개로 제한
 	@Query("""
-			    SELECT COUNT(pb) FROM EventFile pb
-			    WHERE pb.event.progressEndPeriod >= :today
+			    SELECT COUNT(pb) FROM EventBanner pb
+			    WHERE pb.eventInfo.eventEndPeriod >= :today
 			""")
 	long countValidBanners(@Param("today") LocalDate today);
 	
--- backend/src/main/java/com/EduTech/repository/event/EventInfoRepository.java
+++ backend/src/main/java/com/EduTech/repository/event/EventInfoRepository.java
@@ -12,13 +12,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.EduTech.entity.event.Event;
+import com.EduTech.entity.event.EventInfo;
 import com.EduTech.entity.event.EventState;
 
 public interface EventInfoRepository extends JpaRepository<Event, Long> {
 	
 	// 사용자 검색
 	@Query("""
-			SELECT p FROM Event p
+			SELECT p FROM EventInfo p
 			WHERE
 				(
 					(:keyword IS NULL OR :keyword = '')
@@ -36,15 +37,15 @@ public interface EventInfoRepository extends JpaRepository<Event, Long> {
 					)
 				)
 			 	AND (:state IS NULL OR p.state = :state)
-				AND (:progressEndPeriod IS NULL OR p.applyEndPeriod <= :progressEndPeriod)
+				AND (:eventEndPeriod IS NULL OR p.applyEndPeriod <= :eventEndPeriod)
 			""")
-	Page<Event> searchEvent(@Param("searchType") String searchType, @Param("keyword") String keyword,
-							@Param("state") EventState state, @Param("progressStartPeriod") LocalDateTime progressStartPeriod,
-							@Param("progressEndPeriod") LocalDateTime progressEndPeriod, Pageable pageable);
-
+	Page<EventInfo> searchEvent(@Param("searchType") String searchType, @Param("keyword") String keyword,
+							@Param("state") EventState state, @Param("eventStartPeriod") LocalDateTime eventStartPeriod,
+							@Param("eventEndPeriod") LocalDateTime eventEndPeriod, Pageable pageable);
+										// state 오류시 EventState 가 아니라 String 로 변경
 	// 관리자 조건 검색
 	@Query("""
-				SELECT p FROM Event p
+				SELECT p FROM EventInfo p
 				WHERE
 					(
 				    	(:searchType IS NULL OR :searchType = '' OR :keyword IS NULL OR :keyword = '')
@@ -52,15 +53,15 @@ public interface EventInfoRepository extends JpaRepository<Event, Long> {
 				        (:searchType = 'eventName' AND p.eventName LIKE %:keyword%)
 				   	)
 					AND (:state IS NULL OR p.state = :state)
-				 	AND (:progressEndPeriod IS NULL OR p.applyEndPeriod <= :progressEndPeriod)
+				 	AND (:eventEndPeriod IS NULL OR p.applyEndPeriod <= :eventEndPeriod)
 			""")
-	Page<Event> searchAdminEvent(@Param("searchType") String searchType, @Param("keyword") String keyword,
-								 @Param("state") EventState state, @Param("progressStartPeriod") java.time.LocalDateTime progressStartPeriod,
-								 @Param("progressEndPeriod") java.time.LocalDateTime progressEndPeriod, Pageable pageable);
+	Page<EventInfo> searchAdminEvent(@Param("searchType") String searchType, @Param("keyword") String keyword,
+								 @Param("state") EventState state, @Param("eventStartPeriod") java.time.LocalDateTime eventStartPeriod,
+								 @Param("eventEndPeriod") java.time.LocalDateTime eventEndPeriod, Pageable pageable);
 	
 	// 관리자 복합 검색
 	@Query("""
-				SELECT p FROM Event p
+				SELECT p FROM EventInfo p
 				WHERE
 					(:eventName IS NULL OR p.eventName LIKE %:eventName%)
 					AND (:eventInfo IS NULL OR p.eventInfo LIKE %:eventInfo%)
@@ -68,6 +69,6 @@ public interface EventInfoRepository extends JpaRepository<Event, Long> {
 	Page<Event> searchEvent(@Param("eventName") String eventName, @Param("eventInfo") String eventInfo, Pageable pageable);
 	
 	// 지정된 날짜보다 같거나 이후인 행사를 정렬
-	List<Event> findByProgressEndPeriodGreaterThanEqual(LocalDate progressEndPeriod, Sort sort); 
+	List<Event> findByEventEndPeriodGreaterThanEqual(LocalDate eventEndPeriod, Sort sort); 
 	
 }
