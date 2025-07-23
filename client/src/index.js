07413f0c9a46bbc270e9520a10458c98c74adde4
Nam-yongjin <oj4263@naver.com>
2025-07-23 14:27:50 +0900

    카카오 로그인

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RecoilRoot>
-        <App />
-    </RecoilRoot>
+    <Provider store={store}>
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
+    </Provider>
 );
--- client/src/pages/member/KakaoRedirectPage.js
+++ client/src/pages/member/KakaoRedirectPage.js
@@ -1,31 +1,32 @@
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
-import { getAccessToken,getMemberWithAccessToken  } from "../../api/kakaoApi";
+import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
 import moveToPath from "../../hooks/useMove";
-import login from "../../slices/loginSlice";
+import {login} from "../../slices/loginSlice";
 import { useDispatch } from "react-redux";
 
 // 인가 코드의 페이지 처리
 const KakaoRedirectPage = () => {
 
     const [searchParams] = useSearchParams()
-    const authCode = searchParams.get("code") 
-    const dispatch=useDispatch()
+    const authCode = searchParams.get("code")
+    const dispatch = useDispatch()
 
     // 인가 코드가 변경되었을 때 getAccessToken()을 호출
     useEffect(() => {
-        console.log("🔁 useEffect 실행됨")
-        console.log("authCode:", authCode)
         getAccessToken(authCode).then(accessToken => {
-            getMemberWithAccessToken(accessToken).then(memberInfo=>{
+            getMemberWithAccessToken(accessToken).then(memberInfo => {
                 dispatch(login(memberInfo))
+            }).catch(err => {
+                console.error("❌ getMemberWithAccessToken 실패2:", err)
             })
+        }).catch(err => {
+            console.error("❌ getAccessToken 실패2:", err)
         })
     }, [authCode])
 
     return (
         <div>
-            <div>Kakao Login Redirect</div>
         </div>
     )
 }
