677abd4648820f75be960b56552dc2df6f18232e
kimtaegun <tee1694@naver.com>
2025-07-24 17:23:17 +0900

    Admin

--- backend/src/main/java/com/EduTech/service/admin/AdminService.java
+++ backend/src/main/java/com/EduTech/service/admin/AdminService.java
@@ -2,12 +2,16 @@ package com.EduTech.service.admin;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.EduTech.dto.Page.PageResponseDTO;
 import com.EduTech.dto.admin.AdminMemberViewReqDTO;
 import com.EduTech.dto.admin.AdminMemberViewResDTO;
 import com.EduTech.dto.admin.AdminMessageDTO;
+import com.EduTech.dto.admin.BoardListDTO;
 import com.EduTech.dto.demonstration.DemonstrationApprovalRegDTO;
 import com.EduTech.dto.demonstration.DemonstrationApprovalResDTO;
+import com.EduTech.dto.notice.NoticeCreateRegisterDTO;
 import com.EduTech.entity.member.MemberState;
 
 public interface AdminService {
