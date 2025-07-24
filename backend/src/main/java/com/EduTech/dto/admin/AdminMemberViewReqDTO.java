f22bb2bb0ae8459d19006eef001fbfa4a93c5fa4
kimtaegun <tee1694@naver.com>
2025-07-24 18:48:52 +0900

    Admin 테스트

--- backend/src/main/java/com/EduTech/dto/admin/AdminMemberViewReqDTO.java
+++ backend/src/main/java/com/EduTech/dto/admin/AdminMemberViewReqDTO.java
@@ -1,17 +1,17 @@
 package com.EduTech.dto.admin;
 
-import java.time.LocalDateTime;
-
 import com.EduTech.entity.member.MemberRole;
 import com.EduTech.entity.member.MemberState;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // 관리자 회원 정보 페이지에서 필터링 처리위해
 // 값 받아오는 dto (프론트->백)
 // 실제 정보가 아니라 어떤 필드가 변햇는지를 알려줌
 // 바뀐 값만 값 넣어서 옴.
 @Data
+@NoArgsConstructor
 public class AdminMemberViewReqDTO {
 	private String memId; // 회원 아이디
 	private String name; // 회원 이름
