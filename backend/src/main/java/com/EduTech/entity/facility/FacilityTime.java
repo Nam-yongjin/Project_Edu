716a0d1223c5ee4d7edca5782a73d26521e6c0e6
Skatos <Skatos@DESKTOP-87S0C3J>
2025-07-27 21:35:16 +0900

    예약 시간관련 코드

--- backend/src/main/java/com/EduTech/entity/facility/FacilityTime.java
+++ backend/src/main/java/com/EduTech/entity/facility/FacilityTime.java
@@ -3,50 +3,35 @@ package com.EduTech.entity.facility;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "facility_time")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class FacilityTime {
 
-	@Id
-	@Column(nullable = false) //시간번호
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long facTimeNum;
-	
-	@Column(nullable = false) //날짜
-	private LocalDate facDate;
-	
-	@Column(nullable = false) //시작시간
-	private LocalTime startTime;
-	
-	@Column(nullable = false) //종료시간
-	private LocalTime endTime;
-	
-	@Enumerated(EnumType.STRING) //예약상태
-	@Column(nullable = false)
-	private FacilityState state;
-	
-	@ManyToOne(fetch = FetchType.LAZY) //하나의 시설에 여러 개의 시간 예약 가능
-	@JoinColumn(name = "facilityNum", nullable = false) //장소번호
-	private Facility facility;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long facTimeNum;
+
+    private LocalDate facDate;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private boolean available; // 예약 가능 여부
+
+    @ManyToOne
+    @JoinColumn(name = "facility_num")
+    private Facility facility;
 }
