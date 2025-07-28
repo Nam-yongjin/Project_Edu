5bd2e754f76fff424b20c30c3e8c4a8f1fd411aa
kimtaegun <tee1694@naver.com>
2025-07-28 09:38:17 +0900

    Demonstration Front

--- client/src/router/demonstrationRouter.js
+++ client/src/router/demonstrationRouter.js
@@ -2,18 +2,26 @@ import Loading from "./Loading";
 import { Suspense, lazy } from "react";
 
 
+const UpdateDem = lazy(() => import("../pages/demonstration/UpdateDemPage"))
+const AddDem = lazy(() => import("../pages/demonstration/AddDemPage"))
+const DemDetail = lazy(() => import("../pages/demonstration/DemDetailPage"))
+const DemList = lazy(() => import("../pages/demonstration/DemListPage"))
+
 const demonstrationRouter = () => {
 
     return [
         {
             path: "addDem",
-            element: <Suspense fallback={<Loading />}></Suspense>,
+            element: <Suspense fallback={<Loading />}><AddDem /></Suspense>,
         },
         {
             path: "updateDem",
-            element: <Suspense fallback={<Loading />}></Suspense>,
+            element: <Suspense fallback={<Loading />}><UpdateDem /></Suspense>,
         },
-
+        {
+            path:"demDetail",
+            element: <Suspense fallback={<Loading />}><DemDetail /></Suspense>
+        },
     ]
 
 }
