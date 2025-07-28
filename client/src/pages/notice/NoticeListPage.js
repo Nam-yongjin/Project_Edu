09a5d2d2732e378edffbe17a0960907bc8ee889e
miyeon Kim <aldus9714@naver.com>
2025-07-28 16:39:26 +0900

    NoticePage, Router

---
+++ client/src/pages/notice/NoticeListPage.js
@@ -0,0 +1,10 @@
+const NoticeListPage = () => {
+    return (
+        <div>
+            <h1>공지사항 목록 페이지</h1>
+            <NoticeListPage/>
+        </div>
+    )
+}
+
+export default NoticeListPage;
