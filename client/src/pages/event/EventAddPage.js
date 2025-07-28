73760a7716fae4879370f4231c7e1b88eb59cf4c
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-28 18:30:39 +0900

    기초코드

---
+++ client/src/pages/event/EventAddPage.js
@@ -0,0 +1,9 @@
+const EventAddPage = () => {
+    return (
+        <div>
+            <h1>행사 추가 페이지</h1>
+            <br></br>
+        </div>
+    );
+}
+export default EventAddPage;
--- client/src/router/eventRouter.js
+++ client/src/router/eventRouter.js
@@ -4,18 +4,24 @@ import { Suspense, lazy } from "react";
 
 const EventList = lazy(() => import("../pages/event/EventListPage"))
 const EventDetail = lazy(() => import("../pages/event/EventDetailPage"))
+const EventAddPage = lazy(() => import("../pages/event/EventAddPage"))
 
 const eventRouter = () => {
 
     return [
         {
-             path:"evList",
-            element: <Suspense fallback={<Loading />}><evList /></Suspense>,
-        } ,
+             path:"List",
+            element: <Suspense fallback={<Loading />}><EventList /></Suspense>,
+        },
          {
-            path:"evDetail",
-            element: <Suspense fallback={<Loading />}><evDetail /></Suspense>,
+            path:"Detail",
+            element: <Suspense fallback={<Loading />}><EventDetail /></Suspense>,
+        },
+        {
+            path:"Add",
+            element: <Suspense fallback={<Loading />}><EventAddPage /></Suspense>,
         }
+
     ]
 
 }
