53a664d1d97495ae2d46f6c686941b1ffbbb707c
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-28 17:34:05 +0900

    코드수정 완(이제부터 시작)

---
+++ client/src/pages/event/eventPage.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import BasicLayout from "../../layouts/BasicLayout"
+
+const eventPage = () => {
+    return (
+        <div>
+            <BasicLayout>
+                <Outlet />
+            </BasicLayout>
+        </div>
+    )
+}
+export default eventPage
