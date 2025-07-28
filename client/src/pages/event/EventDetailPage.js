8c4a4263130c3a23c168ba2a60775b93b34b9210
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-28 17:20:32 +0900

    Event기초 코드 추가

---
+++ client/src/pages/event/EventDetailPage.js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+    return (
+        <div>
+            <h1>행사  상세 페이지</h1>
+            <br></br>
+        </div>
+    );
+}
+export default EventDetailPage;
---
+++ client/src/pages/event/EventListPage.js
@@ -0,0 +1,9 @@
+const EventListPage = () => {
+    return (
+        <div>
+            <h1>행사 리스트 페이지</h1>
+            <br></br>
+        </div>
+    );
+}
+export default EventListPage;
