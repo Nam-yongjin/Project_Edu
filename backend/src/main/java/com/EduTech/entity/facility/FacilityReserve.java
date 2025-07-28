2d91d15df1ebaac0ced9ead1d7f9b2adcb956581
JA303 <JA303@DESKTOP-EH4LO0B>
2025-07-28 15:05:05 +0900

    오타수정

--- backend/src/main/java/com/EduTech/entity/facility/FacilityReserve.java
+++ backend/src/main/java/com/EduTech/entity/facility/FacilityReserve.java
@@ -14,11 +14,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor  //기본 생성자 추가
+@AllArgsConstructor
 @Entity
 @Table(name = "facility_reserve")
+@Builder
 @Getter
 @Setter
 public class FacilityReserve {
