bf26f14d052eb25d55cb5bbeca22632385fe57b0
Nam-yongjin <oj4263@naver.com>
2025-07-29 14:30:09 +0900

    root 변경

--- backend/src/main/java/com/EduTech/security/jwt/JWTFilter.java
+++ backend/src/main/java/com/EduTech/security/jwt/JWTFilter.java
@@ -1,14 +1,10 @@
 package com.EduTech.security.jwt;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
