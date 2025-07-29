a40f8caa5648cc82bc63af5c38863b07f7581bc7
Nam-yongjin <oj4263@naver.com>
2025-07-29 14:46:25 +0900

    JWT수정

--- backend/src/main/java/com/EduTech/security/jwt/JWTFilter.java
+++ backend/src/main/java/com/EduTech/security/jwt/JWTFilter.java
@@ -33,10 +33,11 @@ public class JWTFilter extends OncePerRequestFilter {
 			Map<String, Object> claims = JWTProvider.validateToken(accessToken);
 
 			String memId = (String) claims.get("memId");
+			String pw = (String) claims.get("pw");
 			String email = (String) claims.get("email");
 			String role = (String) claims.get("role");
 
-			MemberDTO memberDTO = new MemberDTO(memId, email, role);
+			MemberDTO memberDTO = new MemberDTO(memId, pw, email, role);
 
 			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberDTO,
 					null, memberDTO.getAuthorities());
