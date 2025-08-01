1e557f314d3a982bbafb14e92c41d44619d6b5cf
kimtaegun <tee1694@naver.com>
2025-08-01 14:44:13 +0900

    Demonstration Front

---
+++ client/src/components/demonstration/ImageSliderModal.js
@@ -0,0 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+const ImageSliderModal = ({ open, onClose, imageList }) => {
+    if (!open) return null;
+
+    return (
+        <div className="fixed inset-0 bg-white bg-opacity-30 backdrop-blur-sm flex items-center justify-center z-50">
+            <div className="bg-white p-4 rounded-md relative w-[600px] max-w-full">
+                <button
+                    className="absolute top-2 right-2 text-gray-600 z-50 bg-white rounded-full p-1"
+                    onClick={onClose}
+                >
+                    ✕
+                </button>
+
+                {/* Swiper 슬라이더 시작 */}
+                <Swiper
+                    spaceBetween={10}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    className="flex justify-center bg-white"
+                >
+                    {imageList.map((url, idx) => (
+                        <SwiperSlide key={idx}>
+                            <img
+                                src={url}
+                                alt={`preview-${idx}`}
+                                className="max-w-full max-h-[400px] object-contain rounded-md block mx-auto"
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                {/* Swiper 슬라이더 끝 */}
+            </div>
+        </div>
+    );
+};
+
+export default ImageSliderModal;
