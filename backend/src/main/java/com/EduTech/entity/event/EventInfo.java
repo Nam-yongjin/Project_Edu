b727730f5b16bee9377e2e5f17af0754946b3321
Skatos <Skatos@DESKTOP-87S0C3J>
2025-08-03 21:20:10 +0900

    코드 정리

--- backend/src/main/java/com/EduTech/entity/event/EventInfo.java
+++ backend/src/main/java/com/EduTech/entity/event/EventInfo.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
@@ -14,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
