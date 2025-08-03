0640e01ac1a589954bc555eb16871e347d06a4a8
Skatos <Skatos@DESKTOP-87S0C3J>
2025-08-03 21:20:33 +0900

    행사수정 페이지

--- client/src/pages/event/EventUpdatePage.js
+++ client/src/pages/event/EventUpdatePage.js
@@ -2,12 +2,13 @@ import EvtTitleComponent from "../../components/event/EvtTitleComponent";
 import "react-datepicker/dist/react-datepicker.css";
 import EvtUpdateComponent from "../../components/event/EvtUpdateComponent";
 import { useParams } from "react-router-dom";
+
 const UpdatEvtPage = () => {
-    const {evtNum}=useParams();
+    const {eventNum}=useParams();
     return (
         <div className="w-full">
             <EvtTitleComponent title="행사 수정" />
-            <EvtUpdateComponent EvyNum={evtNum}/>
+            <EvtUpdateComponent eventNum={eventNum}/>
         </div>
     );
 }
