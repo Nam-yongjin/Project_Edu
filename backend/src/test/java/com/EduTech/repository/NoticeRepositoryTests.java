55ac65a1dbe6395f411b8ea8b79941f91b2a537c
miyeon Kim <aldus9714@naver.com>
2025-08-04 19:49:21 +0900

    NoticeController 수정 noticeNum인식 오류

--- backend/src/test/java/com/EduTech/repository/NoticeRepositoryTests.java
+++ backend/src/test/java/com/EduTech/repository/NoticeRepositoryTests.java
@@ -177,7 +177,7 @@ public class NoticeRepositoryTests {
         assertThat(update.getNoticeFiles().get(0).getOriginalName()).isEqualTo("new.png");
 	}
 	
-//	@Test
+//	@Test //첨부파일 삭제 실패...
 	@Transactional
 	@DisplayName("공지사항 삭제")
 	@Commit
