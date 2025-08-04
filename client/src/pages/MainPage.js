701a81f9315d6c3f253c7a5a829e42dec9f10a7c
Nam-yongjin <oj4263@naver.com>
2025-08-04 19:12:30 +0900

    CSS

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,6 @@
 import BasicLayout from "../layouts/BasicLayout";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
@@ -15,7 +15,7 @@ const MainPage = () => {
     return (
         <div className="">
             <BasicLayout isFullWidth={true}>
-                <div className="mx-auto">
+                <div className="mx-auto relative">
                     <Swiper
                         spaceBetween={30}
                         centeredSlides={true}
@@ -26,23 +26,32 @@ const MainPage = () => {
                         pagination={{
                             clickable: true,
                         }}
-                        navigation={true}
-                        modules={[Autoplay, Pagination, Navigation]}
+                        modules={[Autoplay, Pagination]}
                         className="mySwiper max-h-screen aspect-[4/3]"
                     >
                         <SwiperSlide>
-                            <img src={main1}/>
+                            <img src={main1} className="w-full h-full object-cover" />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src={main2}/>
+                            <img src={main2} className="w-full h-full object-cover" />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src={main3}/>
+                            <img src={main3} className="w-full h-full object-cover" />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src={main4}/>
+                            <img src={main4} className="w-full h-full object-cover" />
                         </SwiperSlide>
                     </Swiper>
+
+                    <div className="absolute top-1/4 left-0 w-full z-20 pointer-events-none select-none ">
+                        <div className="max-w-screen-xl mx-auto px-4">
+                            <div className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold drop-shadow-[1px_1px_0px_rgba(0,0,0,0.8)]">
+                                서울<br />
+                                에듀테크<br />
+                                소프트랩
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="my-10 max-w-screen-xl mx-auto">
                     <div className="my-10">소개 요약 칸</div>
