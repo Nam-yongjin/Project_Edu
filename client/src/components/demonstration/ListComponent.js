5c48f826a4f84838c163666f4c52c89c32809827
kimtaegun <tee1694@naver.com>
2025-08-06 10:25:38 +0900

    Demonstration Front

--- client/src/components/demonstration/ListComponent.js
+++ client/src/components/demonstration/ListComponent.js
@@ -19,35 +19,9 @@ const ListComponent = () => {
   const [selectedImages, setSelectedImages] = useState([]);
   const { moveToPath } = useMove();
 
-<<<<<<< HEAD
   // ✅ 검색 조건 추가
   const [searchType, setSearchType] = useState("demName");
   const [keyword, setKeyword] = useState("");
-=======
-    return (
-        <>
-            <div className="w-full">
-                <div className="flex gap-2">
-                    {listData.content.map((item) => {
-                        const mainImageObj = item.imageList.find(img => String(img.isMain) === "true");
-                        const mainImageUrl = mainImageObj ? `http://localhost:8090/view/${mainImageObj.imageUrl}` : defaultImg;
-                        return (
-                            <div
-                                key={item.demNum}
-                                className="flex-1 h-[500px] bg-blue-200 p-4 flex flex-col items-center justify-center gap-4 text-center"
-                            >
-                                <img
-                                    onClick={() => {
-                                        const urlList = item.imageList.map(img => `http://localhost:8090/view/${img.imageUrl}`);
-                                        setSelectedImages(urlList);
-                                        setModalOpen(true);
-                                    }}
-                                    src={mainImageUrl}
-                                    alt={`equipment-${item.demNum}`}
-                                    className="w-40 h-[200px] object-cover cursor-pointer"
-                                />
-                                <div className="w-full h-1 bg-black mt-2"></div>
->>>>>>> refs/heads/Demonstration
 
   // ✅ 검색 적용된 리스트 가져오기
   useEffect(() => {
