c9fcd30ee4c60244d6abe97a1b69120a3e3a70d1
JA303 <JA303@DESKTOP-EH4LO0B>
2025-08-06 09:55:35 +0900

    프로그램으로 변경

--- client/src/layouts/Header.js
+++ client/src/layouts/Header.js
@@ -50,14 +50,9 @@ const Header = () => {
       name: "행사",
       link: "/event",
       sub: [
-<<<<<<< HEAD
-        { name: "프로그램 일정", link: "/event" },
-        { name: "프로그램 운영", link: "/" },
-=======
         { name: "행사안내", link: "event/list" },
         { name: "메뉴2", link: "/" },
         { name: "메뉴3", link: "/" },
->>>>>>> refs/heads/Event
       ],
     },
     {
