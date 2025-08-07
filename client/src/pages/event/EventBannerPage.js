5196728b9f22bdcd831936d439b88c064c9820af
Skatos <Skatos@DESKTOP-87S0C3J>
2025-08-08 01:31:39 +0900

    코드 수정

--- client/src/pages/event/EventBannerPage.js
+++ client/src/pages/event/EventBannerPage.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import SubAdminHeader from "../../layouts/SubAdminHeader";
-=======
->>>>>>> refs/heads/Event
 import "react-datepicker/dist/react-datepicker.css";
 import EvtBannerListComponent from "../../components/event/EvtBannerListComponent";
 import SubAdminHeader from "../../layouts/SubAdminHeader";
@@ -9,11 +5,7 @@ import SubAdminHeader from "../../layouts/SubAdminHeader";
 const EventBannerPage = () => {
     return (
         <div>
-<<<<<<< HEAD
-            <SubAdminHeader title="행사 배너" />
-=======
             <SubAdminHeader />
->>>>>>> refs/heads/Event
             <EvtBannerListComponent />
         </div>
     );
