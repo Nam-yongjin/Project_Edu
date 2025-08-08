c7cb9ffa7c8959ab8ae3ce41f0d02ae8ab739b38
JA303 <JA303@DESKTOP-EH4LO0B>
2025-08-08 17:36:50 +0900

    작업중...

---
+++ client/src/api/facilityApi.js
@@ -0,0 +1,17 @@
+//import axios from "axios";
+import jwtAxios from "../util/jwtUtil";
+import { API_SERVER_HOST } from "./config";
+import { API_MAPPING } from "./config";
+
+const host = `${API_SERVER_HOST}/api`;
+const facility = `${host}${API_MAPPING.facility}`;
+
+// 시설 등록 요청 (JWT 필요)
+export const registerFacility = async (formData) => {
+  const res = await jwtAxios.post(`${facility}/register`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return res.data;
+};
---
+++ client/src/pages/facility/FacilityAddPage.js
@@ -0,0 +1,15 @@
+import FacilityTitleComponent from "../../components/facility/FacilityTitleComponent";
+import FacilityAddComponent from "../../components/facility/FacilityAddComponent"
+import "react-datepicker/dist/react-datepicker.css";
+
+const FacilityAddPage = () => {
+
+  return (
+    <div className="w-full">
+      <FacilityTitleComponent title="wkdth 등록" />
+      <FacilityAddComponent />
+      </div>
+  );
+};
+
+export default FacilityAddPage;
