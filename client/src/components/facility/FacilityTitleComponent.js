31128f18c32fc929c42ca5dbf305c2d5e826cc3d
JA303 <JA303@DESKTOP-EH4LO0B>
2025-08-08 17:36:50 +0900

    작업중...

---
+++ client/src/components/facility/FacilityTitleComponent.js
@@ -0,0 +1,9 @@
+const FacilityTitleComponent = ({ title }) => {
+  return (
+    <div className="w-full mt-4 mb-8 px-4 ">
+      <h1 className="text-3xl font-bold mb-2">{title}</h1>
+      <hr className="w-3/5 border-t-2 border-gray-300" />
+    </div>
+  );
+};
+export default FacilityTitleComponent;
