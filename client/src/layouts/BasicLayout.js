1348f2d0cf1423827d9f6ba75ea6b82bd4cd6891
Nam-yongjin <oj4263@naver.com>
2025-08-08 09:13:21 +0900

    CSS

--- client/src/layouts/BasicLayout.js
+++ client/src/layouts/BasicLayout.js
@@ -5,7 +5,7 @@ const BasicLayout = ({ children, isFullWidth = false }) => {
     return (
         <div>
             <Header />
-            <div className={`pt-20 bg-white w-full h-full flex flex-col md:flex-row md:space-x-1 md:space-y-0`}>
+            <div className={`pt-20 bg-white w-full h-full flex flex-col md:flex-row lg:space-x-1`}>
                 <main className={`${isFullWidth ? 'w-full' : 'max-w-screen-xl mx-auto'} main-content `}>
                     {children}
                 </main>
