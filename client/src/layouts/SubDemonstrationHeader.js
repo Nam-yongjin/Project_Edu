82521e5012d4c43c1dceb26649a9e85c8964c9ad
kimtaegun <tee1694@naver.com>
2025-08-12 11:12:44 +0900

    Demonstration Front 실증 소개 페이지 헤더 추가

---
+++ client/src/layouts/SubDemonstrationHeader.js
@@ -0,0 +1,72 @@
+import { Link, useLocation } from "react-router-dom";
+import admin from "../assets/admin.png";
+
+const SubDemonstrationHeader = () => {
+    const location = useLocation();
+
+    return (
+        <div className="bg-red-100 w-full pt-8 pb-8">
+            <div className="max-w-screen-xl mx-auto ">
+                <div className="min-blank flex items-center justify-between">
+                    <div className="newText-4xl font-bold text-gray-800">
+                        실증
+                    </div>
+                    <div className="w-1/4">
+                        <img src={admin} className="w-[400px] p-4" />
+                    </div>
+                </div>
+
+                <div className="newText-lg min-blank border-b-2 border-blue-500 pb-4">
+                    <ul className="flex">
+                        <li className="mr-6 pb-2">
+                            <Link
+                                to={`/admin/members`}
+                                className={`font-bold text-gray-700 hover:text-blue-400 active:text-blue-600${location.pathname.includes("/members") ? "text-blue-600 border-b-2 border-blue-600 active:text-blue-600" : ""
+                                    }`}
+                            >
+                                회원관리
+                            </Link>
+                        </li>
+                        <li className="mr-6 pb-2">
+                            <Link
+                                to="/admin/banner"
+                                className={`font-bold text-gray-700 hover:text-blue-400 active:text-blue-600${location.pathname.includes("/admin/banner") ? "text-blue-600 border-b-2 border-blue-600 active:text-blue-600" : ""
+                                    }`}
+                            >
+                                배너관리
+                            </Link>
+                        </li>
+                        <li className="mr-6 pb-2">
+                            <Link
+                                to="/admin/stats"
+                                className={`font-bold text-gray-700 hover:text-blue-400 active:text-blue-600${location.pathname.includes("/admin/stats") ? "text-blue-600 border-b-2 border-blue-600 active:text-blue-600" : ""
+                                    }`}
+                            >
+                                통계확인
+                            </Link>
+                        </li>
+                        <li className="mr-6 pb-2">
+                            <Link
+                                to="/admin/adminReg"
+                                className={`font-bold text-gray-700 hover:text-blue-400 active:text-blue-600${location.pathname.includes("/admin/adminReg") ? "text-blue-600 border-b-2 border-blue-600 active:text-blue-600" : ""
+                                    }`}
+                            >
+                                실증신청관리
+                            </Link>
+                        </li>
+                        <li className="mr-6 pb-2">
+                            <Link
+                                to="/admin/adminRes"
+                                className={`font-bold text-gray-700 hover:text-blue-400 active:text-blue-600${location.pathname.includes("/admin/adminRes") ? "text-blue-600 border-b-2 border-blue-600 active:text-blue-600" : ""
+                                    }`}
+                            >
+                                실증대여관리
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default SubDemonstrationHeader;
