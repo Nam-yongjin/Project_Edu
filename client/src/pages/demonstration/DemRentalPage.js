076e4956b7eafcf3196a918899ce281d1a50f64c
kimtaegun <tee1694@naver.com>
2025-08-18 18:51:41 +0900

    Demonstration

--- client/src/pages/demonstration/DemRentalPage.js
+++ client/src/pages/demonstration/DemRentalPage.js
@@ -1,18 +1,13 @@
 import RentalComponent from "../../components/demonstration/RentalComponent";
-<<<<<<< HEAD
 import DemTitleComponent from "../../components/demonstration/DemTitleComponent";
 import SubMyInfoHeader from "../../layouts/SubMyInfoHeader";
-=======
->>>>>>> refs/heads/Demonstration
 
 const DemRentalPage =()=> {
     return (
     <>
-<<<<<<< HEAD
+
     <SubMyInfoHeader />
     <DemTitleComponent title="대여 현황" />
-=======
->>>>>>> refs/heads/Demonstration
     <RentalComponent/>
     </>
     );
