6610a7592ad1658715661160574b13bb79a9b1e3
kimtaegun <tee1694@naver.com>
2025-08-18 18:51:41 +0900

    Demonstration

--- client/src/pages/demonstration/DemBorrowPage.js
+++ client/src/pages/demonstration/DemBorrowPage.js
@@ -1,18 +1,13 @@
 import BorrowComponent from "../../components/demonstration/BorrowComponent";
-<<<<<<< HEAD
+
 import DemTitleComponent from "../../components/demonstration/DemTitleComponent";
 import SubMyInfoHeader from "../../layouts/SubMyInfoHeader";
 
-=======
->>>>>>> refs/heads/Demonstration
 const DemDetailPage = () => {
     return (
         <>
-<<<<<<< HEAD
             <SubMyInfoHeader />
             <DemTitleComponent title="실증 물품 목록" />
-=======
->>>>>>> refs/heads/Demonstration
             <BorrowComponent />
         </>
     );
