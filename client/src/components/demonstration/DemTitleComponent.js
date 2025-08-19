38c7fb6ad8656b3cbe0e60b9aed716a493c062b2
kimtaegun <tee1694@naver.com>
2025-08-19 16:01:35 +0900

    Demonstration Front css 수정

--- client/src/components/demonstration/DemTitleComponent.js
+++
@@ -1,9 +0,0 @@
-const DemTitleComponent = ({ title }) => {
-  return (
-    <div className="w-4/5 mt-4 mb-8 px-4 ml-[120px]">
-      <h1 className="text-3xl font-bold mb-2">{title}</h1>
-      <hr className="w-full border-t-2 border-gray-300" />
-    </div>
-  );
-};
-export default DemTitleComponent;
