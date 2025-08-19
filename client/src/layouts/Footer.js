6bfadfd6cfba0f2b29ca2dcb1643528c75e658de
Nam-yongjin <oj4263@naver.com>
2025-08-19 16:44:00 +0900

    Footer 수정

--- client/src/layouts/Footer.js
+++ client/src/layouts/Footer.js
@@ -25,17 +25,31 @@ const Footer = () => {
 
     return (
         <div>
+            
             <footer className="bg-gray-600 py-6">
-                <div className="max-w-screen-xl mx-auto flex justify-between items-start flex-wrap">
+                <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
+                    
                     {/* 왼쪽: 정책 + 주소 */}
-                    <div className='min-blank space-y-4 '>
+                    <div className='space-y-4 text-center md:text-left min-blank'>
                         {/* 정책 */}
-                        <nav className='space-x-2 newText-sm text-white font-medium'>
-                            <span className='hover:underline cursor-pointer ' onClick={() => openModal("register.txt", "이용약관")}><Link to="">이용약관</Link></span>
+                        <nav className='flex flex-wrap justify-center md:justify-start gap-2 newText-sm text-white font-medium'>
+                            <span 
+                                className='hover:underline cursor-pointer' 
+                                onClick={() => openModal("register.txt", "이용약관")}
+                            >
+                                이용약관
+                            </span>
                             <span>|</span>
-                            <span className='hover:underline cursor-pointer' onClick={() => openModal("process.txt", "개인정보처리방침")}><Link to="">개인정보처리방침</Link></span>
+                            <span 
+                                className='hover:underline cursor-pointer' 
+                                onClick={() => openModal("process.txt", "개인정보처리방침")}
+                            >
+                                개인정보처리방침
+                            </span>
                             <span>|</span>
-                            <span className='hover:underline cursor-pointer '><Link to="/about/direction">오시는길</Link></span>
+                            <Link to="/about/direction" className='hover:underline cursor-pointer'>
+                                오시는길
+                            </Link>
                         </nav>
                         {/* 주소 */}
                         <address className="newText-base not-italic space-y-1 text-gray-400">
@@ -45,12 +59,14 @@ const Footer = () => {
                             <p>E-mail : seouledtech@konkuk.ac.kr</p>
                         </address>
                     </div>
-                    {/* 오른쪽 */}
-                    <div className='min-blank text-right mt-6 md:mt-0'>
-                        <img src={logo} alt="로고" className="ml-8 w-[240px] h-[100px] flex-shrink-0" />
+
+                    {/* 오른쪽: 로고 + 저작권 */}
+                    <div className='text-center md:text-right min-blank'>
+                        <img src={logo} alt="로고" className="mx-auto md:ml-8 w-[180px] md:w-[240px] h-auto flex-shrink-0" />
                         {/* https://www.keris.or.kr/main/cm/cntnts/cntntsViewPop.do?cntntsId=1681 */}
-                        <p className='pt-5 text-xs font-light text-gray-300'>
-                            Copyright © 2024 서울에듀테크소프트랩 All Right Reserved.
+                        <p className='pt-4 newText-xs font-light text-gray-300'>
+                            Copyright © 2024 서울에듀테크소프트랩 <br className="md:hidden" />
+                            All Right Reserved.
                         </p>
                     </div>
                 </div>
